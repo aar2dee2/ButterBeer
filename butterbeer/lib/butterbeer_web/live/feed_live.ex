69dda9662211b6a57253cd8bd7da34d547f56753
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-11-29 18:36:31 +0000

    minor edits in FeedLive, updated todos in tomorrow.md

--- butterbeer/lib/butterbeer_web/live/feed_live.ex
+++ butterbeer/lib/butterbeer_web/live/feed_live.ex
@@ -1,28 +1,53 @@
 defmodule ButterbeerWeb.FeedLive do
+  use ButterbeerWeb, :live_view
+  
   def mount(_params, _session, socket) do
+    #Need to add diffs to be tracked    
     {
       :ok,
       assign(
-        socket,
-        #Need to add diffs to be tracked
-      )
+        socket, 
+        :time, DateTime.utc_now() |> to_string()
+        )
     }
   end
 
   def render(assigns) do
-    #Need to add components to template below
+    #Need to remove the Routes.. paths
     ~H"""
-    <h1>
-      Feed
-    </h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Place</th>
+          <th>Description</th>
+          <th>User</th>
+          <th>View on Google Maps</th>
+        </tr>
+      </thead>
+      <tbody>
+        <%= for dine_post <- @dineposts do %>
+          <tr>
+            <td><%= dine_post.place_id %></td>
+            <td><%= dine_post.description %></td>
+            <td><%= dine_post.user_id %></td>
+            <td><%= dine_post.url %></td>
+            <td>
+              <!-- Remove the Routes... path-->
+              <span><%= link "Show", to: Routes.dine_post_path(@conn, :show, dine_post) %></span>
+              <span><%= link "Edit", to: Routes.dine_post_path(@conn, :edit, dine_post) %></span>
+              <span><%= link "Delete", to: Routes.dine_post_path(@conn, :delete, dine_post), method: :delete, data: [confirm: "Are you sure?"] %></span>
+            </td>
+          </tr>
+        <% end %>
+      </tbody>
+    </table>
 
-    <h2>
-     
-    </h2>
-
-    <h2>
-      
-    </h2>
+    <span>
+      <button type="button">
+      <!-- Remove the Routes... path-->
+        <%= link "New Dine post", to: Routes.dine_post_path(@conn, :new) %>
+      </button>
+    </span>
     
     """
   end
--- tomorrow.md
+++ tomorrow.md
@@ -7,8 +7,8 @@
 3. Changes in Feed and DinePost
       - user_id should be passed to the DinePost changeset function when creating a new DinePost
       - In new DinePost template,
-        1. option to select country from dropdown - link to countries schema
-        2. autocomplete search for admin_area_one and admin_area_two - directly included the requisite JS in the template. 
-        3. autocomplete search for place
-        4. When submit button is clicked - write to location schemas in decreasing order of admin levels - first to areas_level_one, then areas_level_two, then localities, then neighborhoods, and finally places.
-        5. remove edit DinePost option for now - can make all pages except `edit` accessible in the router.ex file
+        1. autocomplete search for place
+        2. When submit button is clicked - write to location schemas in decreasing order of admin levels - first to areas_level_one, then areas_level_two, then localities, then neighborhoods, and finally places.
+        3. remove edit DinePost option for now - can make all pages except `edit` accessible in the router.ex file
+
+4. Create and add Butter Beer logo
