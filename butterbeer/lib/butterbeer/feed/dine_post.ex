2dc171e3325fb7941192973271d0675840bdebc3
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-12-10 08:18:49 +0000

    fix: update flow for DinePost.changeset/2

--- butterbeer/lib/butterbeer/feed/dine_post.ex
+++ butterbeer/lib/butterbeer/feed/dine_post.ex
@@ -19,12 +19,16 @@ defmodule Butterbeer.Feed.DinePost do
   end
 
   #The cast function above so make changes to several tables in the db - 
-  # 1/ Add to areas_level_one - long_name, short_name and country_id
-  # 2/ Add to areas_level_two - long_name, short_name and area_level_one (id)
-  # 3/ Add to localities - long_name, short_name, area_level_two (id)
-  # 4/ Add to neighborhoods - long_name, short_name, locality_id
-  # 5/ Add to places - name, url, google_maps_link, neighborhood (id)
-  # 6/ Add to dineposts - description (from form), place_id (refer to places), user_id(refer to user)
+  # get_country_id for the country
+  # check if admin_area_one for the country is there in the areas_level_one database for the specific country
+  # a) if admin_area_one is there - fetch it's id
+  # b) if not there - create an entry with - long_name, short_name and country_id
+  # If case is a) check if admin_area_two is in the areas_level_two  - 
+  # If there go for c) fetch it's id
+  # If not there for d) create entry for admin_area_two with - long_name, short_name and area_level_one (id)
+  # if case is b) - do for d) create entry in areas_level_two
+  # Add to places - name, url, google_maps_link, areas_level_two (id)
+  # Add to dineposts - description (from form), place_id (refer to places), user_id(refer to user)
   # Most of the above should be covered by existing functions in the Location context.
   # So first need to fetch correct country_id from countries - this is done by Location.get_country_id/1
   # So not all places have localities and neighborhoods information. Infact some have sublocalities instead of localities. To work around this, going to just use the areas_level_one and areas_level_two tables for now. In place, can add a column to reference areas_level_two by id. So filtration will be possible on areas areas_level_one and areas_level_two
